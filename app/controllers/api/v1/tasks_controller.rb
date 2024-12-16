e8fe4c7a06c746d103af9ec34c3fa8ef6fb18bcd
Dustin Peukert <dpeukert04@gmail.com>
2024-12-15 21:10:34 -0500

    add task post functionality

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::TasksController < ApplicationController
   def show
     render json: Task.find(params[:id])
   end
+
+  def create
+    render json: Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
